682208b724764f2cb4e68ed1d9dd0286af69fe57
Rodrigo Aranda <raranda@ucatolica.edu.co>
2023-05-10 17:08:35 -0500

    se adiciona clase

---
+++ ClaseRaf.h
@@ -0,0 +1,26 @@
+#ifndef CLASERAF_H
+#define CLASERAF_H
+
+class ClaseRaf
+{
+	public:
+	protected:
+};
+
+#endif
+
+
+/*
+#include <iostream> // Example 8.16
+int main()
+{
+int *p[3], i = 10, j = 30;
+p[0] = &i;
+*p[0] = 20;
+p[1] = &j;
+p[2] = p[0];
+std::cout << i+*p[1]+*p[2] << '\n';
+return 0;
+}
+
+*/
