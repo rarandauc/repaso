7a23a837313027c6f60d7fe1652c1938ab2ddab4
Miguel Vargas <mavargas04@ucatolica.edu.co>
2023-05-10 21:41:53 -0500

    agrego mi clase

---
+++ mavargas04.cpp
@@ -0,0 +1,9 @@
+#include "mavargas04.h"
+
+mavargas04::mavargas04()
+{
+}
+
+mavargas04::~mavargas04()
+{
+}
---
+++ mavargas04.h
@@ -0,0 +1,12 @@
+#ifndef MAVARGAS04_H
+#define MAVARGAS04_H
+
+class mavargas04
+{
+	public:
+		mavargas04();
+		~mavargas04();
+	protected:
+};
+
+#endif
