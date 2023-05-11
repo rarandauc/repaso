873864c786601ae1cf57ebff1288678c9ec113d8
MateoMV <kmmunoz93@ucatolica.edu.co>
2023-05-10 21:26:23 -0500

    agrego mi clase Mateo

---
+++ kmmunoz93.cpp
@@ -0,0 +1,9 @@
+#include "kmmunoz93.h"
+
+kmmunoz93::kmmunoz93(int number)
+{
+}
+
+kmmunoz93::~kmmunoz93()
+{
+}
---
+++ kmmunoz93.h
@@ -0,0 +1,12 @@
+#ifndef KMMUNOZ93_H
+#define KMMUNOZ93_H
+
+class kmmunoz93
+{
+	public:
+		kmmunoz93(int number);
+		~kmmunoz93();
+	protected:
+};
+
+#endif
